018f14c2a79a30f33b909cafa883f6a8503d2f17
Olivianm <144073206+Olivianm@users.noreply.github.com>
2025-04-25 20:16:25 +0000

    modified the name and password of the db

--- View_Folder/purchase_meal.php
+++ View_Folder/purchase_meal.php
@@ -3,8 +3,8 @@ session_start();
 
 // Database Connection
 $host = "localhost";
-$user = "root";
-$password = "";
+$user = "phpmyadmin";
+$password = "P2litmaG";
 $dbname = "ashesismartdiner";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
